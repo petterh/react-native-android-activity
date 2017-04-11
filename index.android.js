c008077907ccf908d86906d682fc84b8768237e9
Petter Hesselberg <petterh@microsoft.com>
2017-04-11 12:05:31 +0200

    Rename React Native component

--- index.android.js
+++ index.android.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class Activity extends Component {
+export default class ActivityDemoComponent extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('ActivityDemo', () => Activity);
+AppRegistry.registerComponent('ActivityDemoComponent', () => ActivityDemoComponent);
