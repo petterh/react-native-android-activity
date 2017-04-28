bfc060264e68ed08ab8e9046d610583bc9f4c872
Petter Hesselberg <petter@petterhesselberg.com>
2017-04-28 09:18:57 +0200

    Added custom native module and sample native activity (#2)
    * Create README.md
    * Check yarn.lock into git: https://github.com/yarnpkg/yarn/issues/1583
    * Rename React Native component
    * Added custom native module and sample native activity
    * Updated README.md; added clipboard support; improved layout.
    * Removed some unused files

---
+++ android/app/src/main/java/com/demo/activity/ActivityStarterModule.java
@@ -0,0 +1,52 @@
+package com.demo.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Expose Java to JavaScript.
+ */
+class ActivityStarterModule extends ReactContextBaseJavaModule {
+
+    ActivityStarterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    /**
+     * @return the name of this module. This will be the name used to {@code require()} this module
+     * from JavaScript.
+     */
+    @Override
+    public String getName() {
+        return "ActivityStarter";
+    }
+
+    @ReactMethod
+    void navigateToExample() {
+        ReactApplicationContext context = getReactApplicationContext();
+        Intent intent = new Intent(context, ExampleActivity.class);
+        context.startActivity(intent);
+    }
+
+    @ReactMethod
+    void dialNumber(@Nonnull String number) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number));
+        getReactApplicationContext().startActivity(intent);
+    }
+
+    @ReactMethod
+    void getActivityName(@Nonnull Callback callback) {
+        Activity activity = getCurrentActivity();
+        if (activity != null) {
+            callback.invoke(activity.getClass().getSimpleName());
+        }
+    }
+}
