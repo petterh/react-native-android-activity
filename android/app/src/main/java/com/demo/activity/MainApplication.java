b55a24c7e4ba3be4487e818432dc984c9223c0ef
Petter Hesselberg <petterh@microsoft.com>
2017-05-11 11:02:17 +0200

    Removing redundancies

--- android/app/src/main/java/com/demo/activity/MainApplication.java
+++ android/app/src/main/java/com/demo/activity/MainApplication.java
@@ -21,7 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
+            return Arrays.asList(
                     new ActivityStarterReactPackage(),
                     new MainReactPackage()
             );
