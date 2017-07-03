d0f5b1ea529a3daf7aabdedab3de6ede392a22e6
Petter Hesselberg <petterh@microsoft.com>
2017-07-03 11:36:16 +0200

    Added custom dev menu item sample.

--- android/app/src/main/java/com/demo/activity/MainActivity.java
+++ android/app/src/main/java/com/demo/activity/MainActivity.java
@@ -1,6 +1,13 @@
 package com.demo.activity;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.devsupport.interfaces.DevOptionHandler;
+import com.facebook.react.devsupport.interfaces.DevSupportManager;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +18,23 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "ActivityDemoComponent";
+    }
+
+    /**
+     * Demonstrates how to add a custom option to the dev menu.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MainApplication application = (MainApplication) getApplication();
+        ReactNativeHost reactNativeHost = application.getReactNativeHost();
+        ReactInstanceManager reactInstanceManager = reactNativeHost.getReactInstanceManager();
+        DevSupportManager devSupportManager = reactInstanceManager.getDevSupportManager();
+        devSupportManager.addCustomDevOption("Custom dev option", new DevOptionHandler() {
+            @Override
+            public void onOptionSelected() {
+                Toast.makeText(MainActivity.this, "Hello from custom dev option", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
