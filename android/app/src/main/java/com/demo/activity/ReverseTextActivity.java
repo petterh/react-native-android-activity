e51c69f7c5a17d5a0a6fd7c06642218a14fce1e8
Petter Hesselberg <petterh@microsoft.com>
2019-03-28 18:33:33 +0100

    Additional comments

--- android/app/src/main/java/com/demo/activity/ReverseTextActivity.java
+++ android/app/src/main/java/com/demo/activity/ReverseTextActivity.java
@@ -10,7 +10,12 @@ import android.widget.TextView;
  * Handles custom edit menu "Reverse string" item.
  * https://medium.com/androiddevelopers/custom-text-selection-actions-with-action-process-text-191f792d2999
  *
- * (This has nothing to do with React Native, to be sure.)
+ * This functionality has nothing to do with React Native per se; I added this just to verify that
+ * the mechanism works as expected on a React Native {@code TextInput}.
+ *
+ * I'm not aware of any comparable iOS functionality -- this thing works globally. iOS has something
+ * you can apply to individual{@code UITextView}s:
+ * https://stackoverflow.com/questions/37870889/how-do-i-add-a-custom-action-to-the-text-selection-edit-menu-in-ios
  */
 public class ReverseTextActivity extends Activity {
 
